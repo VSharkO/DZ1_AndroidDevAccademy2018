8b44887453f0100eec55dd44b2bf1fbd7fcf8c4c
VSharkO <valentin.saric@hotmail.com>
2018-04-09 13:18:26 +0200

    tasks 7. and 8.

---
+++ src/Zadatak7.java
@@ -0,0 +1,20 @@
+public class Zadatak7 {
+    public static void main(String[] args) {
+        //7. Write a method that returns the elements on odd positions in a array
+
+        int[] arr = {-10,12,13,-14,15,16,22,99,87,28,38,56,43,-46,13};
+        int arrR[]=getOddElements(arr);
+        System.out.print("Odd elements of array: ");
+        for(int i=0;i<arr.length/2;i++)
+            System.out.print(arrR[i]+" ");
+    }
+    static int[] getOddElements(int arr[]){
+        int tmpLenght = arr.length/2,j=0;
+        int[] tmp = new int[tmpLenght];
+        for (int i=1; j<tmpLenght; i+=2){
+            tmp[j] = arr[i];
+            j++;
+        }
+        return tmp;
+    }
+}
---
+++ src/Zadatak8.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+
+public class Zadatak8 {
+    public static void main(String[] args) {
+        //78. Write a method that takes a int number and returns a array of its digits.
+        int num=182;
+        int arrR[]=getNumDiggits(num);
+        System.out.print("Digits: ");
+        for(int i=0;i<arrR.length;i++)
+            System.out.print(arrR[i]+" ");
+    }
+    static int[] getNumDiggits(int num){
+        int temp,rez[];
+        ArrayList<Integer> Digits = new ArrayList<>();
+        while(num%10>0){
+            temp=num%10;
+            num/=10;
+            Digits.add(temp);
+        }
+        rez=new int[Digits.size()];
+        int j=0;
+        for(int i=rez.length-1;i>=0;i--){
+            rez[j]=Digits.get(i);
+            j++;
+        }
+        return rez;
+    }
+
+}
+
+
